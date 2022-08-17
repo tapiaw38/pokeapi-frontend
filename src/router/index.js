877fc2876debbdfa8ff4f46189c025807adfc130
walter <walter.tapia@bigbox.com.ar>
2022-08-17 08:16:55 -0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createMemoryHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 import pokemonRouter from '../modules/pokemon/router';
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createMemoryHistory(),
   routes
 })
 
